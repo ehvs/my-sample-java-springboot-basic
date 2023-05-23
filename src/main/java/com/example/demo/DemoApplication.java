1d04431020ae5f17c2e8770393f0c9c067752279
Hevellyn <hevs159@outlook.com>
2023-05-23 20:07:22 +0200

    Update DemoApplication.java
    breaking the app intentionally

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
+// @RequestMapping("/")
+    @RequestMapping
     String home() {
         return "Hello World!";
     }
